3e7d0f2be7abe03da13e8ac91a8d520821d6120e
Donato Rotunno <donato.rotunno@liip.ch>
2013-10-06 22:05:44 +0200

    Add package files

---
+++ src/Lightgear/Theme/ThemeServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Lightgear\Theme;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
